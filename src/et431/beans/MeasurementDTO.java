cdfc2351dd50a0ad717cbe905e0474d25368a08f
sylkat <kryzoxy@gmail.com>
2026-07-03 18:34:38 +0800

    -Implemented derivated values from measurements.

--- src/et431/beans/MeasurementDTO.java
+++ src/et431/beans/MeasurementDTO.java
@@ -1,20 +1,61 @@
 package et431.beans;
 
+import et431.enums.DerivateCapacitance;
+import et431.enums.DerivateImpedance;
+import et431.enums.DerivateInductance;
+import et431.enums.DerivateResistance;
+
+import java.util.Map;
+
 public class MeasurementDTO {
+    private final String measureType;
     private final String typeA;
     private final String valueA;
     private final String typeB;
     private final String valueB;
+    private final Map<DerivateResistance, Double> resistanceDerivator;
+    private final Map<DerivateCapacitance, Double> capacitanceDerivator;
+    private final Map<DerivateInductance, Double> inductanceDerivator;
+    private final Map<DerivateImpedance, Double> impedanceDerivator;
 
-    public MeasurementDTO(String typeA, String valueA, String typeB, String valueB) {
+    public MeasurementDTO(String measureType,
+                          String typeA,
+                          String valueA,
+                          String typeB,
+                          String valueB,
+                          Map<DerivateResistance, Double> resistanceDerivator,
+                          Map<DerivateCapacitance, Double> capacitanceDerivator,
+                          Map<DerivateInductance, Double> inductanceDerivator,
+                          Map<DerivateImpedance, Double> impedanceDerivator) {
+        this.measureType=measureType;
         this.typeA = typeA;
         this.valueA = valueA;
         this.typeB = typeB;
         this.valueB = valueB;
+        this.resistanceDerivator=resistanceDerivator;
+        this.capacitanceDerivator=capacitanceDerivator;
+        this.inductanceDerivator=inductanceDerivator;
+        this.impedanceDerivator=impedanceDerivator;
     }
-
+    public String getMeasureType() { return measureType; }
     public String getTypeA() { return typeA; }
     public String getValueA() { return valueA; }
     public String getTypeB() { return typeB; }
     public String getValueB() { return valueB; }
+
+    public Map<DerivateResistance, Double> getResistanceDerivator() {
+        return resistanceDerivator;
+    }
+
+    public Map<DerivateCapacitance, Double> getCapacitanceDerivator() {
+        return capacitanceDerivator;
+    }
+
+    public Map<DerivateInductance, Double> getInductanceDerivator() {
+        return inductanceDerivator;
+    }
+
+    public Map<DerivateImpedance, Double> getImpedanceDerivator() {
+        return impedanceDerivator;
+    }
 }
